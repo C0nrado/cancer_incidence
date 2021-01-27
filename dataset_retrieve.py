7ed4e408eddba7d230417e0423f21ae2ee281d79
Conrado <conrado-ss@tutanota.com>
2021-01-27 19:31:18 -0300

    mod dataset_retrieve updated

--- dataset_retrieve.py
+++ dataset_retrieve.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from os.path import exists
 import sys
 from resources.utils import summary_dataset
 import pickle
@@ -7,35 +8,54 @@ class WrongInput(Exception):
     def __init__(self, error):
         pass
 
-def read_dataset():
-    """This function returns the data set from the provided buffer when running the script."""
-
+def parse_argv():
     if len(sys.argv) <= 1:
         raise WrongInput('Missing Buffer path')
     else:
         try:
-            # reading buffer path
-            buffer = sys.argv[1]
+            string = ' '.join(sys.argv[1:]).split('-')
+            split_argv = [tuple(arg.strip().split(' ')) for arg in string if arg != '']
+            args = dict(map(lambda x: (x[0], True) if len(x) == 1 else x, split_argv))
+            if not args.get('force'):
+                args['force'] = False
 
-            df = pd.read_fwf(buffer)
-        
         except:
-            raise WrongInput('Incorrect path.')
+            raise WrongInput('Something went wrong in the given input.')
     
+    if len(set(args.keys()) - set(['i', 'O', 'force'])) > 0:
+        raise WrongInput('Incorrect inputs.')
+    
+    return args
+
+def read_dataset():
+    """This function returns the data set from the provided buffer when running the script."""
+
+    # reading buffer path
+    try:
+        buffer = args['i']
+        df = pd.read_fwf(buffer)
+    except:
+        raise WrongInput('Missing/Wrong input path.')
+   
     return df
 
-col_names = {'Ident':'ID',
-             'Grupo':'Group',
-             'Idade':'Age'}
+def output(df):
 
-group_values = {1: 'FP',
-                2: 'FN',
-                3: 'N',
-                4: 'P'}
+    def save_file():
+        pickle.dump(df, open(args['O'], 'wb'))
+        print('Data set saved in %s'%args['O'])
 
-df = read_dataset() \
-        .rename(col_names, axis=1) \
-        .replace({'Group':group_values})
-
+    # setting output file
+    if 'O' not in args.keys():
+        args['O'] = './output.pkl'
+    
+    if exists(args['O']):
+        if not args['force']:
+            raise WrongInput('File %s already exists.'%args['O']) 
+    
+    save_file()
+        
+args = parse_argv()
+df = read_dataset()
+output(df)
 print(summary_dataset(df))
-pickle.dump(df, open('cancer_dataset.pkl', 'wb'))
